74917af8ed71190737bb51e7823b91e1a48b6a1b
ItzChaBoiGhoza <cguy925@gmail.com>
2025-11-11 17:42:59 -0700

    initiating side bar nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 
 const Navbar = () => {
     return (
-        <nav className="max-w-1/2 mx-auto mt-10 rounded-xl">
-            <div className="flex-1 flex justify-center mr-auto text-[28px]">
+        <nav className="max-w-1/2 mx-auto rounded-xl">
+            <div className="flex-1 flex justify-center mr-auto text-[28px] pt-10">
                 <a href="#" className="mx-3 px-8 py-2 rounded-2xl hover:text-[#708993] text-(--base-color)">About</a>
                 <a href="#" className="mx-3 px-8 py-2 rounded-2xl hover:text-[#708993] text-(--base-color)">Projects</a>
                 <a href="#" className="mx-3 px-8 py-2 rounded-2xl hover:text-[#708993] text-(--base-color)">Contact</a>
